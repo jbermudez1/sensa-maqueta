1635f3194eb74538c838109fdd2cc84ee4b3eff7
Javier B <jbermudez@proyeccione.com>
2016-07-19 17:37:02 -0600

    add views

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,33 @@
 $(document).ready( function (argument) {
 	console.log('ready..')
 
+	// box acces in login
+	$('#access').hide();
+
 	var width = $(window).width();
 	console.log(width)
 	if (width < 992) {
 		mobileDevice();
-
 	};
+
+	$('#apply').click( function () {
+		$('#loginform').hide();
+		$('#access').show();
+	})
 
 })
 
 function mobileDevice () {
-	$('body').on({
-	    'touchmove': function(e) {
-	        e.preventDefault();
-	        e.stopPropagation();
-	    },
-	    'mousewheel': function(e) {
-	        e.preventDefault();
-	        e.stopPropagation();
-	    }
-	})
+	// $('body').on({
+	//     'touchmove': function(e) {
+	//         e.preventDefault();
+	//         e.stopPropagation();
+	//     },
+	//     'mousewheel': function(e) {
+	//         e.preventDefault();
+	//         e.stopPropagation();
+	//     }
+	// })
 
 	$('.circle').click( function () {
 		$('.menu').toggleClass('hiden-nav')
